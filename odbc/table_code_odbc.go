af33b3f2974cbd9c14a6c5a3ed4e8a056f40ebe5
Jon Udell <jon@jonudell.info>
2023-09-09 14:59:51 -0700

    ensure values appear in results

--- odbc/table_code_odbc.go
+++ odbc/table_code_odbc.go
@@ -3,17 +3,21 @@ package odbc
 import (
 	"context"
 
+	//"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"	
+
 )
 
 func tableODBC(ctx context.Context, connection *plugin.Connection) (*plugin.Table, error) {
 	// For demonstration purposes, hardcoding column names.
 	// In a real-world scenario, this would involve introspecting the ODBC source to get the column names/types.
 	cols := []*plugin.Column{
-		{Name: "example_column1", Type: proto.ColumnType_STRING, Description: "Example column 1."},
-		{Name: "example_column2", Type: proto.ColumnType_STRING, Description: "Example column 2."},
+		{Name: "example_column1", Type: proto.ColumnType_STRING, Transform: transform.FromField("example_column1"), Description: "Example column 1."},
+		{Name: "example_column2", Type: proto.ColumnType_STRING, Transform: transform.FromField("example_column2"), Description: "Example column 2."},
 	}
+	
 
 	return &plugin.Table{
 		Name:        "odbc_table",
@@ -28,9 +32,10 @@ func tableODBC(ctx context.Context, connection *plugin.Connection) (*plugin.Tabl
 func dummyList(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
 	// Return a dummy row for testing
 	row := map[string]string{
-		"example_column1": "Value 1",
-		"example_column2": "Value 2",
+		"example_column1": "one",
+		"example_column2": "two",
 	}
+	plugin.Logger(ctx).Error("odbc", "dummyList row", row)
 	d.StreamListItem(ctx, row)
 
 	return nil, nil
